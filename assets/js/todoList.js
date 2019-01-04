14963b9da4d12e86e5f5dea8e267a828b98af042
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-03 23:13:51 -0800

    add new item function, refactor previous listener

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -1,10 +1,18 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
-    })
+    });
     e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    }
 });
