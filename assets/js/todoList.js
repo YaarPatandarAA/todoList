a8e1df2647bb0420d3a4d18f6c6e85072cb93f9b
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-03 23:28:54 -0800

    adding css to render look

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -1,16 +1,16 @@
-$("ul").on("click", "li", function(){
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-$("ul").on("click", "span", function(e){
-    $(this).parent().fadeOut(500, function(){
+$("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     e.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(event){
-    if(event.which === 13){
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
         var todoText = $(this).val();
         $(this).val("");
         $("ul").append("<li><span>X</span> " + todoText + "</li>")
